b8bcb31a4fa35d4e78c0870072cde8fc47f1c6b0
pantherofficial404 <pantherofficial404@gmail.com>
2019-10-01 23:24:43 +0530

    PWA Implemented

---
+++ dist/assests/javascript/script.js
@@ -0,0 +1,88 @@
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.slider');
+  var instances = M.Slider.init(elems, { full_width: true, height: 650, interval: 5000 });
+});
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  if (scroll > 150) {
+    $('#nav').css("background", "#232E35")
+  } else {
+    $('#nav').css("background", "transparent")
+  }
+})
+
+function myMap() {
+  var mapProp = {
+    center: new google.maps.LatLng(26.0876637, 87.2337224),
+    zoom: 18,
+  };
+  var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
+  var marker = new google.maps.Marker({ position: { lat: 26.0876637, lng: 87.2337224 }, map: map });
+
+
+}
+
+$(document).ready(function () {
+  $('.parallax').parallax();
+});
+
+
+$(document).ready(function () {
+  jQuery('#loading').fadeOut(100);
+  var scroll = $(window).scrollTop();
+  if (scroll > 150) {
+    $('#nav').css("background", "#232E35");
+
+  } else {
+    $('#nav').css("background", "transparent");
+  }
+});
+
+$(document).ready(function () {
+  $('.collapsible').collapsible();
+});
+
+$(document).ready(function () {
+  $('.sidenav').sidenav({
+    edge: 'right',
+    closeOnClick: true,
+  });
+  $('.sidenav .closable').click(() => {
+    $('.sidenav').sidenav('close');
+  })
+
+  $(".product-sidenav-dropdown-trigger").dropdown({
+    coverTrigger: false,
+    hover: true,
+    closeOnClick: true,
+    constrainWidth: true,
+  });
+
+  $(".product-dropdown-trigger").dropdown({
+    coverTrigger: false,
+    hover: true,
+    closeOnClick: true,
+    constrainWidth: true
+  });
+
+  $(".services-dropdown-trigger").dropdown({
+    coverTrigger: false,
+    hover: true,
+    closeOnClick: true,
+    constrainWidth: true
+  });
+
+  $(".services-sidenav-dropdown-trigger").dropdown({
+    coverTrigger: false,
+    hover: true,
+    closeOnClick: true,
+    constrainWidth: true
+  });
+
+});
+
+var elem = document.querySelector('.collapsible');
+var instance = M.Collapsible.init(elem, {
+  accordion: true
+});
---
+++ dist/precache-manifest.c4483f27aef8d7cceaeff2a55a9f7829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "22a71a7cd0025ff10a87f58fcb5cff04",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13bfe14496bc1bbb9bfc",
+    "url": "/static/css/main.87061b18.chunk.css"
+  },
+  {
+    "revision": "88d9940befc860188be2",
+    "url": "/static/js/2.fb59b4c5.chunk.js"
+  },
+  {
+    "revision": "13bfe14496bc1bbb9bfc",
+    "url": "/static/js/main.83e4b919.chunk.js"
+  },
+  {
+    "revision": "2f52c6af1848cc762986",
+    "url": "/static/js/runtime~main.cfd3a49a.js"
+  }
+]);
